862726df6d78bc4239827737e361d00f7f4b3b86
Jorge Ponce <jponc026@fiu.edu>
2026-04-16 22:38:06 -0400

    -Minor changes

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -35,11 +35,11 @@ export class MenuService {
     { id: 3, name: 'Milk', cost: 0.05, unit: 'ml', stockAmount: 5000, reorderThreshold: 1000 },
     { id: 4, name: 'Foam', cost: 0.03, unit: 'ml', stockAmount: 3000, reorderThreshold: 500 },
     { id: 5, name: 'Chocolate Syrup', cost: 0.15, unit: 'ml', stockAmount: 2000, reorderThreshold: 400 },
-    { id: 6, name: 'Vanilla Syrup', cost: 0.12, unit: 'ml', stockAmount: 2500, reorderThreshold: 500 },
+    { id: 6, name: 'Vanilla Syrup', cost: 0.12, unit: 'ml', stockAmount: 400, reorderThreshold: 500 },
     { id: 7, name: 'Caramel Syrup', cost: 0.13, unit: 'ml', stockAmount: 2200, reorderThreshold: 450 },
     { id: 8, name: 'Whipped Cream', cost: 0.20, unit: 'grams', stockAmount: 1500, reorderThreshold: 300 },
     { id: 9, name: 'Ice', cost: 0.02, unit: 'grams', stockAmount: 8000, reorderThreshold: 1500 },
-    { id: 10, name: 'Sugar', cost: 0.03, unit: 'grams', stockAmount: 3000, reorderThreshold: 600 }
+    { id: 10, name: 'Sugar', cost: 0.03, unit: 'grams', stockAmount: 500, reorderThreshold: 600 }
   ];
 
   private nextId = 3;
